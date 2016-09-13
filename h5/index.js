14140c4ccbae2f2e8421ac902e01faf8e33c5b41
vell001 <vell001@qq.com>
2016-09-13 10:18:03 +0800

    * 修改audio加载，通过js加载

--- h5/index.js
+++ h5/index.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 	var music_play_btn = $('.play-bg-music'),i=0;
+
+	var sound = new Audio();
+    sound.src = "./Star_Sky.mp3";
+    sound.play();
+
 	setInterval(function(){
 		i++;
 		music_play_btn[0].style.transform= 'rotate('+ i +'deg)';
 	},16);
+
 	music_play_btn.click(function(){
-		if($('.bg-music')[0].paused){
-			$(".bg-music")[0].play();
+		if(sound.paused){
+			sound.play();
 		}else{
-			$('.bg-music')[0].pause();
+			sound.pause();
 		}
 	})
 });
